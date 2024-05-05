0123505c40fb43d60b22c271a122c96a455d8b64
tdreams2 <47804910+tdreams2@users.noreply.github.com>
2024-05-05 18:59:18 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,7 @@ use App\Http\Controllers\HomeController;
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/user/{name}', [UserController::class, 'show']);
 
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
